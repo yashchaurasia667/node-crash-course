9eca9141040cf41441b49da64990aeb441940588
Yash <yashchaurasia630@gmail.com>
2024-07-08 17:21:33 +0530

    completed http module, created a simple router and used thunderclient for API testing

--- server.js
+++ server.js
@@ -1,14 +1,40 @@
-import http from 'http';
+import http from "http";
 
-const server = http.createServer((req, res)=> {
-    // res.setHeader('Content-Type', 'text/html');
-    // res.statusCode = 404;
-    // res.write("<h1>Hello world!</h1>");
-    res.writeHead(500, {'Content-Type': 'application/json'})
-    res.write(JSON.stringify({message: 'Internal Server Error...'}))
-    res.end();
-})
+const server = http.createServer((req, res) => {
+  // res.setHeader('Content-Type', 'text/html');
+  // res.statusCode = 404;
+  // res.write("<h1>Hello world!</h1>");
 
-const PORT= 8000;
-server.listen(PORT, ()=> { console.log(`Server started on port ${PORT}`)
-})
+  // console.log(req.url);
+  // console.log(req.method);
+
+  try {
+    // check if a GET req
+    if (req.method === "GET") {
+      if (req.url === "/") {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end("<h1>Homepage</h1>");
+      } else if (req.url === "/about") {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end("<h1>About page </h1>");
+      } else {
+        res.writeHead(404, { "Content-Type": "text/html" });
+        res.end("<h1>404 page not found</h1>");
+      }
+    } else {
+      throw new Error("Method not allowed");
+    }
+  } catch (err) {
+    res.writeHead(500, "text/html");
+    res.end("<h1>500 Internal Server Error</h1>");
+  }
+
+  //   res.writeHead(500, { "Content-Type": "application/json" });
+  //   res.write(JSON.stringify({ message: "Internal Server Error..." }));
+  //   res.end();
+});
+
+const PORT = process.env.PORT;
+server.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
