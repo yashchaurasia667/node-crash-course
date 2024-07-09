a501c4dbc8e5fa89768914efb607d3e03505c223
Yash <yashchaurasia630@gmail.com>
2024-07-09 23:34:23 +0530

    created a middleware

--- server2.js
+++ server2.js
@@ -9,32 +9,41 @@ const users = [
   { id: 4, name: "Jacob Doe" },
 ];
 
-const server = createServer((req, res) => {
-  if (req.url === "/api/users" && req.method === "GET") {
-    res.setHeader("Content-Type", "application/json");
-    res.write(JSON.stringify(users));
-    res.end();
-  } 
-  else if (req.url.match(/\/api\/users\/([0-9])+)/) && req.method === "GET") {
-    const id = req.url.split("/")[3];
-    const user = users.find((user) => user.id === parseInt(id));
+//Logger middleware
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+};
 
-    res.writeHead(200, { "Content-Type": "application/json" });
-    if (user) {
-      res.write(JSON.stringify(user));
-    } 
-    else {
-      res.statusCode(404);
-      res.write({ message: "User not found" });
+// server and a simple API
+const server = createServer((req, res) => {
+  logger(req, res, () => {
+    if (req.url === "/api/users" && req.method === "GET") {
+      res.setHeader("Content-Type", "application/json");
+      res.write(JSON.stringify(users));
+      res.end();
+    } else if (
+      req.url.match(/\/api\/users\/([0-9]+)/) &&
+      req.method === "GET"
+    ) {
+      const id = req.url.split("/")[3];
+      const user = users.find((user) => user.id === parseInt(id));
+
+      res.setHeader("Content-Type", "application/json");
+      if (user) {
+        res.write(JSON.stringify(user));
+      } else {
+        res.statusCode = 404;
+        res.write(JSON.stringify({ message: "User not found" }));
+      }
+      res.end();
+    } else {
+      res.setHeader("Content-Type", "application/json");
+      res.statusCode = 404;
+      res.write({ message: "Route not found..." });
+      res.end();
     }
-    res.end();
-  } 
-  else {
-    res.setHeader("Content-Type", "application/json");
-    res.statusCode(404);
-    res.write({ message: "Route not found..." });
-    res.end();
-  }
+  });
 });
 
 server.listen(PORT, () => {
